b0398eceeac113805805c397c5f479684aa49626
zebnitskyMax <58050667+zebnitskyMax@users.noreply.github.com>
2020-04-07 21:02:48 +0300

    feat: add onclick on mouse and keydown on keyboard

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ window.onload = function () {
     var textarea = document.createElement('textarea');
     form.appendChild(textarea);
     textarea.setAttribute('class', 'textarea');
+    textarea.setAttribute('id', 'textarea');
     textarea.setAttribute('rows', '10');
 
     var div_inner = document.createElement('div');
@@ -19,33 +20,51 @@ window.onload = function () {
     div_inner.setAttribute('class', 'div_inner');
 
     let arr_firstLine = ['`', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '-', '=', 'Backspace'];
-    function lounchCreatElem(someARR){
-    for (var i = 0; i < someARR.length; i++) {
-        function creatElem(elem) {
-            button = document.createElement('button');
-            div_inner.appendChild(button);
-            button.setAttribute("class", 'button' + someARR[i]);
-            button.setAttribute("id", 'button');
-            button.setAttribute("value", someARR[i]);
-            button.setAttribute("type", 'button');
-           /*  button.setAttribute("id", 'insert([i])'); */
-            button.innerHTML = someARR[i];
-        }
-        creatElem(i);
+
+    function creatElem(elem) {
+        button = document.createElement('button');
+        div_inner.appendChild(button);
+        button.setAttribute("class", 'button' + elem);
+        button.setAttribute("id", 'button');
+        button.setAttribute("value", elem);
+        button.setAttribute("type", 'button');
+
+        button.addEventListener('click', function (event) {
+            event.stopPropagation();
+            insert(event.target.getAttribute('value'));
+        })
+        
+        button.innerHTML = elem;
     }
-}
-lounchCreatElem(arr_firstLine);
+
+
+
+    function lounchCreatElem(someARR) {
+        for (var i = 0; i < someARR.length; i++) {
+            creatElem(someARR[i]);
+        }
+    }
+    lounchCreatElem(arr_firstLine);
     let arr_secondLine = ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'Del'];
     let arr_therdLine = ['CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '/', 'Enter'];
     let arr_fourthLine = ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', 'up', 'Shift'];
     let arr_fifthLine = ['Ctrl', 'Win', 'Alt', 'space', 'Alt', 'left', 'down', 'right', 'Ctrl'];
-lounchCreatElem(arr_secondLine);
-lounchCreatElem(arr_therdLine);
-lounchCreatElem(arr_fourthLine);
-lounchCreatElem(arr_fifthLine);
+    lounchCreatElem(arr_secondLine);
+    lounchCreatElem(arr_therdLine);
+    lounchCreatElem(arr_fourthLine);
+    lounchCreatElem(arr_fifthLine);
 
-button.onclick = function(symbol){
-    document.getElementById('button').value = document.getElementById('button').value + symbol;
-}
-  
+    window.insert = function (symbol) {
+        document.getElementById('textarea').value += symbol;
+    }
+
+    document.addEventListener('keydown', function (event) {
+        event.stopPropagation();
+        insert(event.key);
+        console.log('alt', event.altKey);
+        console.log('shift', event.shiftKey);
+    });
+
+    
+
 }
